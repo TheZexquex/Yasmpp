ee2658778527ce5b3fbcf6c9dc4169ad76a55b6b
TheZexquex <jakobreiser@gmx.de>
2024-11-28 17:04:15 +0100

    feat: add sounds to countdown ends in default settings

--- src/main/java/dev/thezexquex/yasmpp/configuration/settings/CountDownSettings.java
+++ src/main/java/dev/thezexquex/yasmpp/configuration/settings/CountDownSettings.java
@@ -18,8 +18,11 @@ public class CountDownSettings {
             new CountDownEntry(4, true, true, true, "entity.experience_orb.pickup"),
             new CountDownEntry(3, true, true, true, "entity.experience_orb.pickup"),
             new CountDownEntry(2, true, true, true, "entity.experience_orb.pickup"),
-            new CountDownEntry(1, true, true, true, "entity.experience_orb.pickup")
+            new CountDownEntry(1, true, true, true, "entity.experience_orb.pickup"),
+            new CountDownEntry(0, false, false, true, "block.end_portal.spawn")
+
     );
+
     @JsonProperty
     private List<CountDownEntry> gameStartCountDown = List.of(
             new CountDownEntry(15, true, true, true, "entity.experience_orb.pickup"),
@@ -28,16 +31,20 @@ public class CountDownSettings {
             new CountDownEntry(4, true, true, true, "entity.experience_orb.pickup"),
             new CountDownEntry(3, true, true, true, "entity.experience_orb.pickup"),
             new CountDownEntry(2, true, true, true, "entity.experience_orb.pickup"),
-            new CountDownEntry(1, true, true, true, "entity.experience_orb.pickup")
+            new CountDownEntry(1, true, true, true, "entity.experience_orb.pickup"),
+            new CountDownEntry(0, false, false, true, "block.end_portal.spawn")
     );
+
     @JsonProperty
     private List<CountDownEntry> teleportCountDown = List.of(
             new CountDownEntry(5, false, true, true, "entity.experience_orb.pickup"),
             new CountDownEntry(4, false, true, true, "entity.experience_orb.pickup"),
             new CountDownEntry(3, false, true, true, "entity.experience_orb.pickup"),
             new CountDownEntry(2, false, true, true, "entity.experience_orb.pickup"),
-            new CountDownEntry(1, false, true, true, "entity.experience_orb.pickup")
+            new CountDownEntry(1, false, true, true, "entity.experience_orb.pickup"),
+            new CountDownEntry(0, false, false, true, "item.chorus_fruit.teleport")
     );
+
     @JsonProperty
     private List<CountDownEntry> openEndCountDown = List.of(
             new CountDownEntry(15, true, true, true, "entity.experience_orb.pickup"),
@@ -46,7 +53,8 @@ public class CountDownSettings {
             new CountDownEntry(4, true, true, true, "entity.experience_orb.pickup"),
             new CountDownEntry(3, true, true, true, "entity.experience_orb.pickup"),
             new CountDownEntry(2, true, true, true, "entity.experience_orb.pickup"),
-            new CountDownEntry(1, true, true, true, "entity.experience_orb.pickup")
+            new CountDownEntry(1, true, true, true, "entity.experience_orb.pickup"),
+            new CountDownEntry(0, false, false, true, "block.end_portal.spawn")
     );
 
     public CountDownSettings() {
