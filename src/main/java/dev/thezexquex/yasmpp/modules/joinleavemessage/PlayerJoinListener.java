27c68c7717caec2a0af78f949f045daaf8dc8cc7
TheZexquex <contact@thezexquex.dev>
2025-12-19 16:56:10 +0100

    fix: dumb behavior of OfflinePlayer#hasPlayedBefore resulting in true in PlayerJoinEvent even if it is called on a first join of a player leading to the first join message never being triggered.

--- src/main/java/dev/thezexquex/yasmpp/modules/joinleavemessage/PlayerJoinListener.java
+++ src/main/java/dev/thezexquex/yasmpp/modules/joinleavemessage/PlayerJoinListener.java
@@ -3,17 +3,37 @@ package dev.thezexquex.yasmpp.modules.joinleavemessage;
 import de.unknowncity.astralib.common.message.lang.Language;
 import dev.thezexquex.yasmpp.YasmpPlugin;
 import dev.thezexquex.yasmpp.data.adapter.LocationAdapter;
+import io.papermc.paper.event.player.AsyncPlayerSpawnLocationEvent;
 import net.kyori.adventure.text.minimessage.tag.resolver.Placeholder;
+import org.bukkit.Bukkit;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.AsyncPlayerPreLoginEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
 import org.spongepowered.configurate.NodePath;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.UUID;
 
 public class PlayerJoinListener implements Listener {
     private final YasmpPlugin plugin;
+    private final Set<UUID> firstTimeJoiners = new HashSet<>();
 
     public PlayerJoinListener(YasmpPlugin plugin) {
         this.plugin = plugin;
+    }
+
+    @EventHandler
+    public void onPlayerPreLogin(AsyncPlayerPreLoginEvent event) {
+        Bukkit.getScheduler().runTask(plugin, () -> {
+            var offlinePlayer = Bukkit.getOfflinePlayer(event.getUniqueId());
+            if (!offlinePlayer.hasPlayedBefore()) {
+                plugin.getLogger().info("Player " + offlinePlayer.getName() + " has joined for the first time!");
+                firstTimeJoiners.add(event.getUniqueId());
+            }
+        });
     }
 
     @EventHandler
@@ -21,8 +41,10 @@ public class PlayerJoinListener implements Listener {
         var player = event.getPlayer();
 
         plugin.smpPlayerService().addSmpPlayer(player);
+        boolean isFirstTime = firstTimeJoiners.remove(player.getUniqueId());
 
-        if (player.hasPlayedBefore()) {
+        if (!isFirstTime) {
+            firstTimeJoiners.remove(player.getUniqueId());
             var messageComponent = plugin.messenger().component(
                     Language.GERMAN,
                     NodePath.path("event", "join"),
@@ -47,5 +69,11 @@ public class PlayerJoinListener implements Listener {
         spawnLocation.ifPresentOrElse(worldPosition -> {
             player.teleport(LocationAdapter.adapt(worldPosition.locationContainer(), player.getServer()));
         }, () -> plugin.messenger().sendMessage(player, NodePath.path("command", "spawn", "no-spawn")));
+    }
+
+
+    @EventHandler
+    public void onQuit(PlayerQuitEvent event) {
+        firstTimeJoiners.remove(event.getPlayer().getUniqueId());
     }
 }
