4d1f01980ebfd0087c104d51ba5355ed9389590b
TheZexquex <contact@thezexquex.dev>
2025-12-26 12:12:19 +0100

    feat: add portal fix command

--- src/main/java/dev/thezexquex/yasmpp/commands/admin/PortalCommand.java
+++ src/main/java/dev/thezexquex/yasmpp/commands/admin/PortalCommand.java
@@ -7,6 +7,7 @@ import org.bukkit.entity.Player;
 import org.checkerframework.checker.nullness.qual.NonNull;
 import org.incendo.cloud.CommandManager;
 import org.incendo.cloud.context.CommandContext;
+import org.spongepowered.configurate.NodePath;
 
 public class PortalCommand extends PaperCommand<YasmpPlugin> {
     public PortalCommand(YasmpPlugin plugin) {
@@ -21,6 +22,19 @@ public class PortalCommand extends PaperCommand<YasmpPlugin> {
                 .literal("moveHere")
                 .handler(this::handleSummon)
         );
+
+        commandManager.command(commandManager.commandBuilder("portal")
+                .permission("yasmpp.command.portal")
+                .senderType(Player.class)
+                .literal("fix")
+                .handler(this::handleFix)
+        );
+    }
+
+    private void handleFix(@NonNull CommandContext<Player> context) {
+        var player = context.sender();
+        plugin.netherPortalManager().respawnPortalOnServerStart();
+        plugin.messenger().sendMessage(player, NodePath.path("command", "portal", "fix"));
     }
 
     private void handleSummon(@NonNull CommandContext<Player> context) {
